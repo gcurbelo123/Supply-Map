60106780f36fdca68dd32cc92a4c7ff565dc61d8
Luis <lvalencia@csumb.edu>
2017-11-05 01:14:41 -0800

    Needs beautifying

--- mysite/map/urls.py
+++ mysite/map/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
    url(r'^$', landing, name="landing"),
    url(r'^disp$', mapping, name="map"),
-   url(r'^map$', makeMap, name="map")
+   url(r'^map$', makeMap, name="map"),
+   url(r'^staticmap$', staticMap, name ="staticmap")
    
 ]
