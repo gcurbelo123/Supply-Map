fa189ba19fdf6657c6934b412ecd38f23834d8d2
Luis <lvalencia@csumb.edu>
2017-11-05 13:01:21 -0800

    Final version with nav bar

--- mysite/map/urls.py
+++ mysite/map/urls.py
@@ -4,8 +4,10 @@ from .import views
 
 urlpatterns = [
    url(r'^$', landing, name="landing"),
+   url(r'^home$', landing, name="home"),
    url(r'^disp$', mapping, name="map"),
    url(r'^map$', makeMap, name="map"),
-   url(r'^staticmap$', staticMap, name ="staticmap")
+   url(r'^staticmap$', staticMap, name ="staticmap"),
+   url(r'^savePic$', save, name= "savePic")
    
 ]
--- mysite/map/views.py
+++ mysite/map/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import map.screenshot
 
 
 def mapping(request):
    makeMap(request)
    return render(request, "map.html")
 	
-	
+def save(request):
+   map.screenshot.main()
+   return render(request, "map.html")
    
 def makeMap(request):
    
